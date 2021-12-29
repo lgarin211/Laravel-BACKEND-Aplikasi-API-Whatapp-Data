36778643950d605474f1a49f7c10d3a74167f2ad
unknown <lagustinus211@gmail.com>
2021-12-29 15:49:11 +0700

    masukan data

--- app/Http/Controllers/DagurController.php
+++ app/Http/Controllers/DagurController.php
@@ -8,14 +8,41 @@ use Illuminate\Support\Facades\Http;
 
 class DagurController extends Controller
 {
+    
     public function sending()
     {
-        $data=DB::table('dagurs')->get();
+
+        $response = Http::get('https://api.telegram.org/bot5088921065:AAGnV63SM7g4l5gllimnIoZHymBuEh9GovM/getUpdates');
+        $data=$response->json()['result'];
+        $chat=[];
         foreach ($data as $key => $value) {
-            $nomor=$value->nomor;
-            $pesan='hallo '.$value->nama_Guru.' kami mengingatkan anda untuk melakukan absensi di https://www.google.com/';
-            $response = Http::get('http://localhost/WhatsappAPI-php-class/?nomor='.$nomor.'&pesan='.$pesan);
-            dump($value->nama_Guru,'berhasil');
+            $chat[$value['message']['from']['id']]['id']=$value['message']['from']['id'];
+            $chat[$value['message']['from']['id']]['first_name']=$value['message']['from']['first_name'];
         }
+        foreach ($chat as $key => $value) {
+            // dd($value);
+            $this->push($value['id'],$value['first_name']);
+        }
+    
+    }
+    public function push($chatid,$name,$task='Presensi')
+    {
+        // $data=DB::table('dagurs')->get();
+        // foreach ($data as $key => $value) {
+            // $nomor=$value->nomor;
+            $pesan='hallo '.$name.' kami mengingatkan anda untuk melakukan '.$task.' di www.google.com';
+            // $response = Http::get('http://172.16.14.250/WhatsappAPI-php-class/?nomor='.$nomor.'&pesan='.$pesan);
+            // dump($value->nama_Guru,'berhasil');
+            
+            $pesan = json_encode($pesan);
+            $API = "https://api.telegram.org/bot5088921065:AAGnV63SM7g4l5gllimnIoZHymBuEh9GovM/sendmessage?chat_id=".$chatid."&text=$pesan";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_URL, $API);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            return $result; 
+        // }
     }
 }
